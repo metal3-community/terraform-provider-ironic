8d6fd59037fc3d605e2e12648d6dad27b39a8bb7
appkins <nbatkins@gmail.com>
2025-03-10 14:42:06 -0500

    Remove vendor

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+//go:generate terraform fmt -recursive ./examples/
+
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name ironic
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
